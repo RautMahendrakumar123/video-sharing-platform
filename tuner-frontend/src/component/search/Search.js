9c99c8739fd03547ab59cc06ae32fa2b2a210e4d
RautMahendrakumar123 <rautmahendrakumar2@gmail.com>
2023-06-10 10:18:16 +0530

    images added for search functionality

--- tuner-frontend/src/component/search/Search.js
+++ tuner-frontend/src/component/search/Search.js
@@ -13,7 +13,10 @@ const Search = () => {
     "https://upload.wikimedia.org/wikipedia/en/0/07/Rana_Naidu.jpg","https://bloody-disgusting.com/wp-content/uploads/2014/03/godzilla-banner2.jpg",
     "https://e0.pxfuel.com/wallpapers/182/151/desktop-wallpaper-godzilla-king-of-the-monsters-banner-cartoon-godzilla.jpg",
     "https://collider.com/wp-content/uploads/the-avengers-movie-poster-banners-03.jpg",
+    "https://upload.wikimedia.org/wikipedia/en/0/07/Rana_Naidu.jpg",
     "https://upload.wikimedia.org/wikipedia/en/0/07/Rana_Naidu.jpg"]
+
+    
   return (
     <div className='lowerbody-container'>
        {
