b19d9019c7342be00262a13969c5aa64658ef6ae
RautMahendrakumar123 <rautmahendrakumar2@gmail.com>
2023-06-10 08:25:01 +0530

    new search component added

--- tuner-frontend/src/Router.js
+++ tuner-frontend/src/Router.js
@@ -4,6 +4,7 @@ import NavBar from './component/navbar/NavBar'
 import Landing from './component/Landing/Landing'
 import SignIn from './component/signin/SignIn'
 import Register from './component/register/Register'
+import Search from './component/search/Search'
 
 const Router = () => {
   return (
@@ -13,7 +14,7 @@ const Router = () => {
     <Route path='/' element={<Landing />}/>
     <Route path='/Signin' element={<SignIn />}/>
     <Route path='/Register' element={<Register />}/>
-    {/* <Route path='/' element={<Landing />}/> */}
+    <Route path='/search' element={<Search />}/>
    </Routes>
    </BrowserRouter>
   )
