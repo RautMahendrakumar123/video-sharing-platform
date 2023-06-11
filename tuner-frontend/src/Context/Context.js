7040a2fcd170d763ef0ee23fdad7e58e0879ab46
mayank3122 <singhmayank2231@gmail.com>
2023-06-11 15:13:14 +0530

    myvideo connencted to backend

--- tuner-frontend/src/Context/Context.js
+++ tuner-frontend/src/Context/Context.js
@@ -4,11 +4,12 @@ export const context=createContext()
 
 const ContextPro=({children})=>{
   const[inputs, setInputs] = useState('')
+  const[myvideo, setMyvid] = useState([])
    const  [arr,setArray]=useState([])
    const [playvideo,setPlayVideo]=useState("")
   
     return(
-        <context.Provider value={{arr,setArray,playvideo,setPlayVideo, inputs, setInputs}}>
+        <context.Provider value={{arr,setArray,playvideo,setPlayVideo, inputs, setInputs, myvideo, setMyvid}}>
       {children}
         </context.Provider>
     )
